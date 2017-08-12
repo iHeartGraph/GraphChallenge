6ec17a970fad94503aad1bcc435800e68fa34559
Ahsen Uppal <auppal@gwu.edu>
2017-08-12 06:39:38 +0000

    Support parallel benchmarking jobs.

--- StochasticBlockPartition/code/python/run_regression_test.py
+++ StochasticBlockPartition/code/python/run_regression_test.py
@@ -29,6 +29,21 @@ class Bunch(object):
         self.__dict__.update(adict)
 
 
+# See: https://stackoverflow.com/questions/17223301/python-multiprocessing-is-it-possible-to-have-a-pool-inside-of-a-pool
+class NoDaemonProcess(mp.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class NonDaemonicPool(mp.pool.Pool):
+    Process = NoDaemonProcess
+
+
 shortname = {'decimation' : 'd',
              'input_filename' : 'F',
              'iteration' : 'itr',
@@ -57,58 +72,75 @@ def outputname(args_tuple):
     return out
 
 
-def child_func(queue, redirected_stdout, func, args):
+def child_func(queue, fout, func, args):
     rc = 0
-    t0 = timeit.default_timer()
+    wall_time = 0
+    rusage_self = None
+    rusage_children = None
 
-    with redirect_stdout(redirected_stdout):
-        try:
+    try:
+        t0 = timeit.default_timer()
+
+        with redirect_stdout(fout):
             func(args)
-        except:
-            traceback.print_exc()
-            traceback.print_exc(file=redirected_stdout)
-            rc = 1
 
-    t1 = timeit.default_timer()
-    wall_time = t1 - t0
-    redirected_stdout.flush()
-    rusage_self = resource.getrusage(resource.RUSAGE_SELF)
-    rusage_children = resource.getrusage(resource.RUSAGE_CHILDREN)
+        t1 = timeit.default_timer()
+        wall_time = t1 - t0
+        fout.flush()
+
+        rusage_self = resource.getrusage(resource.RUSAGE_SELF)
+        rusage_children = resource.getrusage(resource.RUSAGE_CHILDREN)
+    except:
+        traceback.print_exc()
+        traceback.print_exc(file=fout)
+        rc = 1
+
     queue.put((rc, wall_time, rusage_self, rusage_children))
     sys.exit(rc)
 
-def profile_child(redirected_stdout, func, args):
+def profile_child(out_dir, func, args):
+    outname = out_dir + '/' + outputname(args)
+    fout = open(outname, 'w')
+    args = Bunch(args)
     queue = mp.Queue()
-    p = Process(target=child_func, args=(queue, redirected_stdout, func, args))
+    p = Process(target=child_func, args=(queue, fout, func, args))
     p.start()
     rc,wall_time,rusage_self,rusage_children = queue.get()
     p.join()
-    return rc,wall_time,rusage_self,rusage_children
+    return outname,rc,wall_time,rusage_self,rusage_children
 
-def run_test(out_dir, base_args, input_files, iterations, threads):
+def profile_wrapper(tup):
+    (out_dir, args) = tup
+    return profile_child(out_dir, do_main, args)
+
+def run_test(out_dir, base_args, input_files, iterations, threads, max_jobs = 1):
     results = {}
 
-    for input_filename,thread,iteration in itertools.product(input_files, threads, iterations):
-        args = base_args.copy()
-        args['input_filename'] = input_filename
-        args['threads'] = thread
-        args_sorted = tuple(sorted((i for i in args.items()))) + (('iteration', iteration),)
+    work_list = [i for i in itertools.product(input_files, threads, iterations)]
 
-        outname = out_dir + '/' + outputname(args_sorted)
+    arg_list = [base_args.copy() for i in work_list]
 
-        with open(outname, 'w') as f:
-            print("Running " + str(args))
-            rc,t_elp,rusage_self,rusage_children = profile_child(f, do_main, Bunch(args))
-            mem_rss = rusage_self.ru_maxrss + rusage_children.ru_maxrss
+    for i,(input_filename,thread,iteration) in enumerate(work_list):
+        arg_list[i]['input_filename'] = input_filename
+        arg_list[i]['threads'] = thread
+        arg_list[i] = tuple(sorted((j for j in arg_list[i].items()))) + (('iteration', iteration),)
 
-            if rc == 0:
-                print("Took %3.4f seconds and used %d k maxrss" % (t_elp, mem_rss))
-                print("")
-            else:
-                print("Exception occured. Continuing.")
-                print("")
+    pool = NonDaemonicPool(max_jobs)
 
-        results[args_sorted] = outname,t_elp,mem_rss
+    result_list = pool.map(profile_wrapper, [(out_dir, i) for i in arg_list])
+
+    for args,(outname,rc,t_elp,rusage_self,rusage_children) in zip(arg_list, result_list):
+        mem_rss = rusage_self.ru_maxrss + rusage_children.ru_maxrss
+        if rc == 0:
+            print(args)
+            print("Took %3.4f seconds and used %d k maxrss" % (t_elp, mem_rss))
+            print("")
+        else:
+            print("Exception occured. Continuing.")
+            print("")
+
+        results[i] = outname,t_elp,mem_rss
+
     return results
 
 
@@ -122,13 +154,15 @@ if __name__ == '__main__':
                    '../../data/static/simulated_blockmodel_graph_1000_nodes',
                    '../../data/static/simulated_blockmodel_graph_5000_nodes')
 
-    iterations = range(3)
+    iterations = range(56)
 
-    results = run_test(out_dir, base_args, input_files, iterations, threads = (0,))
+    results = run_test(out_dir, base_args, input_files[:1], iterations, threads = (0,), max_jobs = 28)
 
     print("Single process tests.")
     for k,v in sorted((i for i in results.items())):
         print("%s %s" % (v[0],v[1:]))
+
+    sys.exit(0)
 
     results = run_test(out_dir, base_args, input_files, iterations, threads = (4,8,11))
 
